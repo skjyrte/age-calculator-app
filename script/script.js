9253dfbb2dedb318603fb5fd8fc0a85f5c285eed
skjyrte <103749024+skjyrte@users.noreply.github.com>
2023-05-07 17:13:10 +0200

    script modified

--- script/script.js
+++ script/script.js
@@ -1,40 +1,65 @@
-const yearInput = document.querySelector("#year");
-const monthInput = document.querySelector("#month");
-const dayInput = document.querySelector("#day");
 
+dayjs.extend(preciseDiff);
+dayjs.extend(window.dayjs_plugin_customParseFormat);
+
+const input = document.querySelector(".inputArea");
 
 const yearResult = document.querySelector(".resultYear");
 const monthResult = document.querySelector(".resultMonth");
 const dayResult = document.querySelector(".resultDay");
 
+input.addEventListener("input", setValue);
 
-/*
-yearInput.addEventListener("input", calculateValue);
-
-function calculateValue(e) {
-  yearResult.textContent = e.target.value;
+function setValue(e) {
+  birthday[e.target.name] = e.target.value;
+  validationFunc();
 }
-*/
-
 
 //birthday string values
-let year = "1995";
-let month = "11"; 
-let day = "22";
+let birthday = {
+  year: 2000,
+  month: 1,
+  day: 1
+}
 
-//current date
-const currentDate = new Date();
-//birthday date
-const birthdayDate = new Date(`${year}-${month}-${day}T00:00:00`)
-//time difference in miliseconds
+function validationFunc() { 
 
-console.log(currentDate - birthdayDate)
-const timeDiff = currentDate.getTime() - birthdayDate.getTime()
-//number of ms in single day
-const oneDay = 24 * 60 * 60 * 1000; 
-//number of days
-const diffDays = Math.round(Math.abs((timeDiff) / oneDay)); 
+  //---year
+if (birthday.year.length ===! 4 ){
+}//this field is required
+if (birthday.year < 1900){
+}//year must be over 1900
+if (birthday.year > 2023){
+}//must be in past
 
-currentDate.setDate(currentDate.getDate() - diffDays);
+//---month
+if (birthday.month.length ===! 2 ){
+}//this field is required
+if (birthday.month < 1 || birthday.month > 12){
+}//must be a valid month
 
-console.log(currentDate);
+//---day
+if (birthday.day.length ===! 2 ){
+}//this field is required
+if (birthday.day < 1 || birthday.day > 31){
+}//must be a valid day
+
+//---date validity
+if (!dayjs(`${birthday.year}-${birthday.month}-${birthday.day}`, 'YYYY-MM-DD', true).isValid()){
+}//Must be a valid date
+}
+
+
+/*
+
+const birthdayDate = new Date(`${birthday.year}-${birthday.month}-${birthday.day}T00:00:00`)
+
+let m1 = dayjs(birthdayDate);
+let m2 = dayjs(); //current date
+let diff = dayjs.preciseDiff(m1, m2, true);
+
+console.log(diff.years)
+console.log(diff.months)
+console.log(diff.days)
+
+*/
