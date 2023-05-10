bf8e9784b76dbb3d4fdfa4e76ce29d9a82d279c1
skjyrte <103749024+skjyrte@users.noreply.github.com>
2023-05-10 16:04:27 +0200

    script modified

--- script/script.js
+++ script/script.js
@@ -1,78 +1,84 @@
-
 dayjs.extend(preciseDiff);
 dayjs.extend(window.dayjs_plugin_customParseFormat);
 
-const input = document.querySelector(".inputArea");
+//const yearInput = document.querySelector(".yearInput");
+//const monthInput = document.querySelector(".monthInput");
+//const dayInput = document.querySelector(".dayInput");
 
-const yearInput = document.querySelector(".yearInput");
-const monthInput = document.querySelector(".monthInput");
-const dayInput = document.querySelector(".dayInput");
+
+document.querySelectorAll('input[type="number"]').forEach(input =>{
+  input.oninput = () => {
+    if(input.value.length > input.maxLength) input.value = input.value.slice(0, input.maxLength);
+  }
+})
+
 
 const yearResult = document.querySelector(".yearResult");
 const monthResult = document.querySelector(".monthResult");
 const dayResult = document.querySelector(".dayResult");
 
-input.addEventListener("input", setValue);
+
+
+document.querySelector(".inputArea").addEventListener("input", setValue);
 
 function setValue(e) {
   birthday[e.target.name] = e.target.value.toString();
 }
 
-
-//birthday string values
+//birthday object, string values
 let birthday = {
   year: "0",
   month: "0",
-  day: "0"
-}
-
-
+  day: "0",
+};
 
 const currentDate = dayjs();
 
+//VALIDATORS
+const required = (lngt) => (val) => {
+  if (!val) {
+    return "This field is required.";
+  }
 
-//validators
-
-
-const required = (lngt) => (val) => { //val means input value
-  if (!val || val.length !== lngt) {
-    return "This field is required";
+  if (val.length !== lngt) {
+    return "improper date format.";
   }
 
   return null;
 };
 
 
-const isBetweenRange = (min, max) => (val) => { //val means day, month or year
+
+//day, month, year validator
+const isBetweenRange = (min, max) => (val) => {
   if (val < min) {
     return `Value should be at least ${min}.`;
   }
 
   if (val > max) {
-    return `Value should be at most ${max}`;
+    return `Value should be at most ${max}.`;
   }
 
   return null;
 };
 
-
-const isValidDate = (val) => { //val means birthday date
+//birthday date validator
+const isValidDate = (val) => {
   if (!val.isValid()) {
-    return "Must be a valid date";
+    return "Must be a valid date.";
   }
 
   return null;
 };
 
-
-const isBeforeDate = (val) => { //val means birthday date
+//birthday date validator
+const isBeforeDate = (val) => {
   if (!dayjs(val).isBefore(currentDate)) {
-    return "Birthday date must be in the past";
+    return "Birthday date must be in the past.";
   }
 
   return null;
 };
-
 
 const validate = (val, validators) =>
   validators.reduce((errors, currentValidator) => {
@@ -84,45 +90,43 @@ const validate = (val, validators) =>
     return errors;
   }, []);
 
-
 const yearValidators = [required(4), isBetweenRange(1900, 2023)];
 const monthValidators = [required(2), isBetweenRange(1, 12)];
 const dayValidators = [required(2), isBetweenRange(1, 31)];
 const dateValidators = [isValidDate, isBeforeDate];
 
-
 document.querySelector(".image").addEventListener("click", () => {
+  let birthdayDate = dayjs(
+    `${birthday.year}-${birthday.month}-${birthday.day}`,
+    "YYYY-MM-DD",
+    true
+  );
 
-  let birthdayDate = dayjs(`${birthday.year}-${birthday.month}-${birthday.day}`, 'YYYY-MM-DD', true);
-
-  const yearErrors = (validate(birthday.year, yearValidators));
-  const monthErrors = (validate(birthday.month, monthValidators));
-  const dayErrors = (validate(birthday.day, dayValidators));
-
-  console.log(yearErrors);
-  console.log(monthErrors);
-  console.log(dayErrors);
+  const yearErrors = validate(birthday.year, yearValidators);
+  const monthErrors = validate(birthday.month, monthValidators);
+  const dayErrors = validate(birthday.day, dayValidators);
 
   document.querySelector(".yearInput > .error").textContent = yearErrors[0];
   document.querySelector(".monthInput > .error").textContent = monthErrors[0];
   document.querySelector(".dayInput > .error").textContent = dayErrors[0];
 
-  if (yearErrors.length===0 && monthErrors.length===0 && dayErrors.length===0) {
-  const dateErrors = (validate(birthdayDate, dateValidators));
-  console.log(dateErrors);
-  document.querySelector(".dayInput > .error").textContent = dateErrors[0];
+  document.querySelector(".yearResult").innerHTML = "--";
+  document.querySelector(".monthResult").innerHTML = "--";
+  document.querySelector(".dayResult").innerHTML = "--";
+
+  if (
+    yearErrors.length === 0 &&
+    monthErrors.length === 0 &&
+    dayErrors.length === 0
+  ) {
+    const dateErrors = validate(birthdayDate, dateValidators);
+    document.querySelector(".dayInput > .error").textContent = dateErrors[0];
+
+    if (dateErrors.length === 0) {
+      let diff = dayjs.preciseDiff(birthdayDate, currentDate, true);
+      document.querySelector(".yearResult").innerHTML = diff.years;
+      document.querySelector(".monthResult").innerHTML = diff.months;
+      document.querySelector(".dayResult").innerHTML = diff.days;
+    }
   }
-
-  if (yearErrors.length===0 && monthErrors.length===0 && dayErrors.length===0 && dateErrors.length===0) {
-  let diff = dayjs.preciseDiff(birthdayDate, currentDate, true);
-
-  console.log(diff.years)
-  console.log(diff.months)
-  console.log(diff.days)
-  }
-  
-})
-
-
-
-
+});
