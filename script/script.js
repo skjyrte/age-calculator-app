af586c7409ccfb1e40f289619ef7e7978820dfe6
skjyrte <103749024+skjyrte@users.noreply.github.com>
2023-05-13 18:13:17 +0200

    script modified

--- script/script.js
+++ script/script.js
@@ -1,30 +1,29 @@
 "use strict";
-//dayJS plugins
+
 dayjs.extend(preciseDiff);
 dayjs.extend(window.dayjs_plugin_customParseFormat);
-//limit the character count for input type "number"
 
-document.querySelectorAll('input').forEach((input) => {
+document.querySelectorAll("input").forEach((input) => {
   input.oninput = () => {
-    console.log(input.value)
+    console.log(input.value);
     if (input.value == "") {
-      input.value = "";
+      //firefox input type "number" bug fix
+      input.value = ""; //firefox input type "number" bug fix
     }
     if (input.value.length > input.maxLength)
       input.value = input.value.slice(0, input.maxLength);
   };
 });
 
-//fill the birthday object with input values
 document.querySelector(".inputArea").addEventListener("input", setValue);
 function setValue(e) {
-  birthday[e.target.name] = e.target.valueAsNumber; //value is passed a string, asNumber was required
+  birthday[e.target.name] = e.target.valueAsNumber;
   e.target.parentNode.classList.remove("errorStyles");
-  //reset the previous date
+
   document.querySelector(".yearResult").innerHTML = "--";
   document.querySelector(".monthResult").innerHTML = "--";
   document.querySelector(".dayResult").innerHTML = "--";
-  //clear the error field on input
+
   document.querySelector(`.${e.target.name}Input > .error`).textContent = "";
   if (dateErorrsMarker !== 0) {
     document.querySelector(".dayInput").classList.remove("errorStyles");
@@ -33,12 +32,10 @@ function setValue(e) {
   }
 }
 
-//passing function scoped variable to global scoped event listener
 let dateErorrsMarker;
-//initial current date
+
 let currentDate = dayjs();
 
-//birthday object, property value type "number"
 let birthday = {
   year: 0,
   month: 0,
@@ -47,14 +44,12 @@ let birthday = {
 
 /******VALIDATORS******/
 
-//1) if value is present validator
 const required = (val) => {
   if (!val) {
     return "This field is required.";
   }
 };
 
-//2) input correcting validator
 const characterValidator = (lngth, selector, val) => {
   let valStr = val.toString();
   while (valStr.length < lngth) {
@@ -64,7 +59,6 @@ const characterValidator = (lngth, selector, val) => {
   return valStr;
 };
 
-//3) day, month, year validator
 const isBetweenRange = (min, max) => (val) => {
   if (val < min) {
     return `Value should be at least ${min}.`;
@@ -77,7 +71,6 @@ const isBetweenRange = (min, max) => (val) => {
   return null;
 };
 
-//4) birthday date validator according to calendar
 const isValidDate = (val) => {
   if (!val.isValid()) {
     return "Must be a valid date.";
@@ -86,7 +79,6 @@ const isValidDate = (val) => {
   return null;
 };
 
-//5) birthday must be in the past validator
 const isBeforeDate = (val) => {
   if (!dayjs(val).isBefore(currentDate)) {
     return "Birthday date must be in the past.";
@@ -95,7 +87,6 @@ const isBeforeDate = (val) => {
   return null;
 };
 
-//gather the error values into arrays
 const validate = (val, validators) =>
   validators.reduce((errors, currentValidator) => {
     const validationResult = currentValidator(val);
@@ -110,7 +101,6 @@ const yearValidators = [required, isBetweenRange(1900, 2023)];
 const monthValidators = [required, isBetweenRange(1, 12)];
 const dayValidators = [required, isBetweenRange(1, 31)];
 
-//function needed to paint relevant boxes red according to appropriate error type
 function colorError(val, obj) {
   if (obj.length > 0) {
     val.forEach((element) =>
@@ -119,41 +109,37 @@ function colorError(val, obj) {
   }
 }
 
-//events for button color change during hoover action
 document.querySelector(".image>button").addEventListener("mouseover", (e) => {
   e.currentTarget.classList.add("mouseoverClass");
-}); //use of currentTarget was necessary
+});
 
 document.querySelector(".image>button").addEventListener("mouseout", (e) => {
   e.currentTarget.classList.remove("mouseoverClass");
-}); //use of currentTarget was necessary
+});
 
 /******VALIDATION BUTTON******/
 
 document.querySelector(".image>button").addEventListener("click", () => {
-  // fix input value characters function e.g. input 1 -> 01
-  characterValidator(2, "day", birthday.day); //days
-  characterValidator(2, "month", birthday.month); //months
-  characterValidator(4, "year", birthday.year); //years
-  //input numbers validators
+  characterValidator(2, "day", birthday.day);
+  characterValidator(2, "month", birthday.month);
+  characterValidator(4, "year", birthday.year);
+
   const yearErrors = validate(birthday.year, yearValidators);
   const monthErrors = validate(birthday.month, monthValidators);
   const dayErrors = validate(birthday.day, dayValidators);
-  //display only single error note
+
   document.querySelector(".yearInput > .error").textContent = yearErrors[0];
   document.querySelector(".monthInput > .error").textContent = monthErrors[0];
   document.querySelector(".dayInput > .error").textContent = dayErrors[0];
-  //painting red the incorrect fields
+
   colorError(["day"], dayErrors);
   colorError(["month"], monthErrors);
   colorError(["year"], yearErrors);
 
-  //current date need to be updated after every calculation request
   currentDate = dayjs();
-  //clearing previous value
+
   dateErorrsMarker = 0;
 
-  //if numbers are ok validate whole date
   if (
     yearErrors.length === 0 &&
     monthErrors.length === 0 &&
@@ -168,13 +154,11 @@ document.querySelector(".image>button").addEventListener("click", () => {
     const dateErrors = validate(birthdayDate, dateValidators);
     document.querySelector(".dayInput > .error").textContent = dateErrors[0];
 
-    //passing date validation error to the outside scope
     dateErorrsMarker = dateErrors.length;
-    //painting red incorrect fields
+
     colorError(["day", "month", "year"], dateErrors);
-    //if whole date is valid then calculate the result
+
     if (dateErrors.length === 0) {
-      //time difference calculation
       let diff = dayjs.preciseDiff(birthdayDate, currentDate, true);
       document.querySelector(".yearResult").innerHTML = diff.years;
       document.querySelector(".monthResult").innerHTML = diff.months;
